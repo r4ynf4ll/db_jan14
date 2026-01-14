0581474e1bed5c5c91025ea7bb342df97f110c69
Anna Buckel <abuckel0503@gmail.com>
2026-01-14 15:45:24 +0000

    database with table of 3 records

--- create_points.py
+++ create_points.py
@@ -3,6 +3,7 @@ conn = sqlite3.connect('points.db')
 cursor = conn.cursor()
 
 # create table with 3 colums, each point has x and y coordinates with an id
+# a row in the table is called a record
 query = """
 CREATE TABLE IF NOT EXISTS points (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
--- insert.py
+++ insert.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('points.db')
 cursor = conn.cursor()
 query = """
 INSERT INTO points(x,y)
-VALUES (3,5);
+VALUES (8,9);
 """
 
 cursor.execute(query)
