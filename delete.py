37349c194e6be640e471259f3170196432a7a529
Anna Buckel <abuckel0503@gmail.com>
2026-01-16 15:19:14 +0000

    deleted rows after id 3

--- delete.py
+++ delete.py
@@ -1,15 +1,10 @@
 import sqlite3
-
 conn = sqlite3.connect('points.db')
 cursor = conn.cursor()
-query = """
-INSERT INTO points(x,y)
-VALUES
-(2,5),
-(9,1),
-(16,7)
-"""
 
+query = """
+    DELETE FROM points WHERE id > 3;
+"""
 cursor.execute(query)
 conn.commit()
 conn.close()
