218fd71ffc004d62f0df8f1dd31f97cf72d51014
Anna Buckel <abuckel0503@gmail.com>
2026-01-16 15:26:56 +0000

    fetchall

---
+++ fetch.py
@@ -0,0 +1,15 @@
+import sqlite3
+conn = sqlite3.connect('points.db')
+cursor = conn.cursor()
+
+query = """
+    SELECT *
+    FROM points;
+"""
+
+cursor.execute(query)
+# difference is the line below, need to fetch
+results = cursor.fetchall()
+conn.close()
+
+print(results)
