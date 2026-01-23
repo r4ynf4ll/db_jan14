08cbf4fc18d77e07b1b5dca00cc649e1c0b8027c
Anna Buckel <abuckel0503@gmail.com>
2026-01-23 15:35:02 +0000

    df displayed in gradio

--- gradio_practice.py
+++ gradio_practice.py
@@ -1,10 +1,21 @@
 import gradio as gr
+import sqlite3
+import pandas as pd
 
-
-def f(x):
-    return x**2
+def fetch():
+    conn = sqlite3.connect('points.db')
+    cursor = conn.cursor()
+    query = """
+        SELECT *
+        FROM points;
+    """
+    cursor.execute(query)
+    records = cursor.fetchall()
+    conn.close()
+    records_df = pd.DataFrame(records,columns=['id','x','y'])
+    return records_df
 
 # making interface
-iface = gr.Interface(fn = f,inputs="number",outputs="number")
+iface = gr.Interface(fn = fetch(),inputs=[],outputs=gr.Dataframe(headers=['id','x','y']))
 
 iface.launch()
